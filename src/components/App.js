f0736aafc4f03603ed77d8bf5260a2fcbaae0c53
hoaithu842 <hoaithu842@gmail.com>
2024-12-01 12:39:00 +0700

    signup ui

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import {Container} from "react-bootstrap";
+import Signup from "./Signup";
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+
+            <Container
+                className="d-flex align-items-center justify-content-center"
+                style={{minHeight: "100vh"}}>
+                <div className="w-100" style={{maxWidth: "400px"}}>
+                    <Signup/>
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default App;
